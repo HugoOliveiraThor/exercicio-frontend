2ae2e7905d085d056fb7f70105e8ce607a85101f
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2020-09-08 23:49:29 -0300

    adjust warning in span in linkicon

--- src/components/link-icon/linkIcon.js
+++ src/components/link-icon/linkIcon.js
@@ -3,19 +3,23 @@ import { FaRegEdit } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
-export default function LinkIcon({ label }) {
+export default function LinkIcon({ label, handleClick }) {
   return (
     <Wrapper>
       <FaRegEdit color="green" size="0.4em" />
-      <span>{label}</span>
+      <span onClick={handleClick} aria-hidden="true">
+        {label}
+      </span>
     </Wrapper>
   );
 }
 
 LinkIcon.propTypes = {
   label: PropTypes.string,
+  handleClick: PropTypes.func,
 };
 
 LinkIcon.defaultProps = {
   label: '',
+  handleClick: () => {},
 };
