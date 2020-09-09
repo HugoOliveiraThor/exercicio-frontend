8d1bd2ed3e020477ef193e61e6cd791edbeafb40
Hugo Oliveira <hugooliveirahenrique@gmail.com>
2020-09-08 23:48:43 -0300

    Add variables to appjs

--- src/components/button/styles.js
+++ src/components/button/styles.js
@@ -5,9 +5,10 @@ export const StyleBtn = styled.button`
   height: 30px;
   font-family: $font-family;
   background-color: ${(props) =>
-    props.color === 'green' ? 'var(--green)' : '#04a9ff'};
+    props.color === 'green' ? 'var(--green)' : 'var(--blue)'};
   padding: 5px 5px 5px 5px;
   color: white;
   border: none;
   border-radius: 4px;
+  cursor: pointer;
 `;
